e73c10523386fa1aebab41cc950ce3c4650ae11f
Aino Spring <info@aino-spring.com>
2025-12-11 16:59:04 +0100

    Flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+	description = "Label utilities";
+
+	inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { self, nixpkgs, flake-utils, ... }@inputs: flake-utils.lib.eachDefaultSystem (system:
+		let
+			pkgs = import nixpkgs { inherit system; };
+		in
+		{
+			packages.textlabel = pkgs.buildGoModule {
+				pname = "textlabel";
+				version = "0.1.0";
+
+				src = ./.;
+
+				vendorHash = "sha256-ejc+3Gcp1Ax8z6CExigGgl6aXnyOiFSmA+eKWhxLDb0=";
+				subPackages = [ "./textlabel" ];
+			};
+		}
+	);
+}
